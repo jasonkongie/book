0924a7e864427760e559f804687443b1f852ddb4
jckongucsd <jckong@ucsd.edu>
2023-05-25 20:20:46 -0700

    everything is done. Just the time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import { useState } from 'react';
-import { Document, Page } from 'react-pdf';
+import React, { useState, useEffect } from 'react';
+import pdfFile from './book.pdf';  
+import LoadingPage from './components/LoadingPage';
 
-function App() {
-  const [pdfFiles, setPdfFiles] = useState(["pdf1.pdf", "pdf2.pdf"]); // replace with your actual PDF file names
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const visited = localStorage.getItem('visited_gallery');
+
+    if (!visited) {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+        localStorage.setItem('visited_gallery', true);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
 
   return (
-    <div>
-      {pdfFiles.map(pdf => (
-        <Document file={pdf} key={pdf}>
-          <Page pageNumber={1} />
-        </Document>
-      ))}
+    <div className="pdf">
+      {isLoading ? (
+        <div className="gallery-loading">
+          <LoadingPage
+            strings={["Custom text 1.", "Custom text 2."]}
+            typeSpeed={100}
+            backSpeed={50}
+            loop={false}
+          />
+        </div>
+      ) : (
+
+        React.createElement(
+          "div",
+          {},
+          React.createElement("iframe", {
+            src: pdfFile,
+            style: { width: '100%', height: '800px' },
+            frameborder: "0",
+          })
+        )
+
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadingPage.css';
+
+function LoadingPage() {
+  return (
+    <div className="loading-page">
+      My footprint of consciousness. 
+    </div>
+  );
+}
+
+export default LoadingPage;
