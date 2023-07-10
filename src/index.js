404fdb30084a1064c564ed8cd725a1bd93eec644
jckongucsd <jckong@ucsd.edu>
2023-07-10 14:20:12 -0700

    added spotify component.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from './App';
 import MuseumProject from './components/MuseumProject';
+import {Spotify} from './components/spotify/Spotify'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/museum-project" element={<MuseumProject />} />
+        <Route path="/spotify" element={<Spotify />} /> 
       </Routes>
     </Router>
   </React.StrictMode>,
