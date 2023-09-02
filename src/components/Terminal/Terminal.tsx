e2d88a29442635e5272f990ef60e5ad0a1e90b72
jckongucsd <jckong@ucsd.edu>
2023-09-02 20:22:02 +0800

    adjusted terminal -- but still some issues with cut off

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,66 +1,73 @@
+import React, { useEffect, useRef, useState, useCallback, forwardRef, ForwardedRef } from 'react';
 import './terminal.css';
-import React, { ForwardedRef, forwardRef, useCallback, useEffect, useRef, useState } from 'react';
 import { TerminalProps } from "./types";
 
-export const Terminal = forwardRef(
-  (props: TerminalProps, ref: ForwardedRef<HTMLDivElement>) => {
-    const {
-      history = [],
-      promptLabel = '>',
-      commands = {},
-    } = props;
+export const Terminal = forwardRef((props: TerminalProps, ref: ForwardedRef<HTMLDivElement>) => {
+  const {
+    history = [],
+    promptLabel = '>',
+    commands = {},
+  } = props;
 
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [input, setInputValue] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const terminalRef = useRef<HTMLDivElement>(null); // Create a ref for the terminal container
+  const [input, setInputValue] = useState<string>('');
 
-    useEffect(() => {
-      inputRef.current?.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
-    const focusInput = useCallback(() => {
-      inputRef.current?.focus();
-    }, []);
+  useEffect(() => {
+    // Auto-scroll to bottom when history changes
+    if (terminalRef.current) {
+      terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+    }
+  }, [history]);
 
-    const handleInputChange = useCallback(
-      (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value);
-      },
-      []
-    );
+  const focusInput = useCallback(() => {
+    inputRef.current?.focus();
+  }, []);
 
-    const handleInputKeyDown = useCallback(
-      (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-          const commandToExecute = commands?.answer;
-          if (commandToExecute) {
-            commandToExecute(input);
-          }
-          setInputValue('');
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value);
+    },
+    []
+  );
+
+  const handleInputKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        const commandToExecute = commands?.answer;
+        if (commandToExecute) {
+          commandToExecute(input);
         }
-      },
-      [commands, input]
-    );
+        setInputValue('');
+      }
+    },
+    [commands, input]
+  );
 
-    return (
-      <div className="terminal" ref={ref} onClick={focusInput}>
-        {history.map((line, index) => (
-          <div className="terminal__line" key={`terminal-line-${index}-${line}`}>
-            {line}
-          </div>
-        ))}
-        <div className="terminal__prompt">
-          <div className="terminal__prompt__label">{promptLabel}</div>
-          <div className="terminal__prompt__input">
-            <input
-              type="text"
-              value={input}
-              onKeyDown={handleInputKeyDown}
-              onChange={handleInputChange}
-              ref={inputRef}
-            />
-          </div>
+  return (
+    <div className="terminal" ref={terminalRef} onClick={focusInput}> {/* Assign the ref */}
+      {history.map((line, index) => (
+        <div className="terminal__line" key={`terminal-line-${index}-${line}`}>
+          {line}
+        </div>
+      ))}
+      <div className="terminal__prompt">
+        <div className="terminal__prompt__label">{promptLabel}</div>
+        <div className="terminal__prompt__input">
+          <input
+            type="text"
+            value={input}
+            onKeyDown={handleInputKeyDown}
+            onChange={handleInputChange}
+            ref={inputRef}
+          />
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
+
